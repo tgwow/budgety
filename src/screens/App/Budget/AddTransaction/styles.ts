eba537bea1464d680b29dcfc3519f679748ef2d1
Thiago Oliveira <thiago.2809@outlook.com.br>
2021-05-19 23:56:40 -0300

    refactor: cleanup on AddNewTransaction component

--- src/screens/App/Budget/AddTransaction/styles.ts
+++ src/screens/App/Budget/AddTransaction/styles.ts
@@ -2,6 +2,8 @@ import { ScaledSheet } from 'react-native-size-matters';
 
 export default ScaledSheet.create({
   container: {
-    // zIndex: -9,
+    justifyContent: 'space-evenly',
+    paddingHorizontal: '30@s',
   },
+  button: { marginTop: '5@s' },
 });
