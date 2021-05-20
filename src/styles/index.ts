6e8edd7a0737419949dbb65b28cb59682fc06010
Thiago Oliveira <thiago.2809@outlook.com.br>
2021-05-19 23:52:42 -0300

    feat: add some global styles

---
+++ src/styles/index.ts
@@ -0,0 +1,17 @@
+import { ScaledSheet } from 'react-native-size-matters';
+
+export default ScaledSheet.create({
+  input: {
+    marginBottom: '30@vs',
+  },
+  underline: {
+    textDecorationLine: 'underline',
+  },
+  createContainer: {
+    marginTop: '15@vs',
+    alignSelf: 'center',
+  },
+  button: {
+    padding: '20@s',
+  },
+});
